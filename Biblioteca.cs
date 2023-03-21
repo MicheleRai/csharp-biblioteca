a1234ba5f953650ca5460f9748397df10310d4ae
MicheleRai <rainonemichele1999@gmail.com>
2023-03-21 17:06:49 +0100

    fix and improve on the organization end structure of the project

---
+++ Biblioteca.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Biblioteca;
+
+public class Biblioteca
+{
+
+    public static string GenerateId()
+    {
+        var random = Convert.ToString(new Random().Next(1, 999999));
+        while (random.Length < 6)
+        {
+            random = "0" + random;
+        }
+        return random;
+    }
+
+
+    public class User : Biblioteca
+    {
+        private string nome;
+        private string cognome;
+        private string email;
+        private string password;
+        private string telefono;
+
+        public User(string nome, string cognome, string email, string password, string telefono)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.email = email;
+            this.password = password;
+            this.telefono = telefono;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+
+        public override string ToString()
+        {
+            return $"Nome: {nome}" + Environment.NewLine +
+                   $"Cognome:  {cognome}" + Environment.NewLine +
+                   $"Email:  {email}" + Environment.NewLine +
+                   $"Password:  {password}" + Environment.NewLine +
+                   $"Telefono:  {telefono}";
+        }
+    }
+
+    public class Product : Biblioteca
+    {
+
+        private string codice;
+        private string titolo;
+        private int anno;
+        private string settore;
+        private string posizione;
+        private string[] autore = new string[2];
+
+        public Product(string titolo, int anno, string settore, string posizione, string[] autore)
+        {
+            this.codice = GenerateCode();
+            this.titolo = titolo;
+            this.anno = anno;
+            this.settore = settore;
+            this.posizione = posizione;
+            this.autore = autore;
+        }
+
+        public string Titolo { get => titolo; set => titolo = value; }
+        public int Anno { get => anno; set => anno = value; }
+        public string Settore { get => settore; set => settore = value; }
+        public string Posizione { get => posizione; set => posizione = value; }
+        public string[] Autore { get => autore; set => autore = value; }
+        public string GenerateCode()
+        {
+            return GenerateId();
+        }
+        public override string ToString()
+        {
+            return $"Codice identificativo: {codice}" + Environment.NewLine +
+                   $"Titolo:  {titolo}" + Environment.NewLine +
+                   $"Anno:  {anno}" + Environment.NewLine +
+                   $"Settore:  {settore}" + Environment.NewLine +
+                   $"Posizione:  {posizione}" + Environment.NewLine +
+                   $"autore:  {string.Join(" ", autore)}";
+        }
+    }
+
+    public class DVD : Product
+    {
+        int durata;
+        public DVD(string titolo, int anno, string settore, string posizione, string[] autore, int durata) : base(titolo, anno, settore, posizione, autore)
+        {
+            this.durata = durata;
+        }
+
+        public int Durata { get => durata; set => durata = value; }
+
+        public override string ToString()
+        {
+            return base.ToString() + Environment.NewLine +
+                $"Durata min: {durata}";
+        }
+    }
+
+    public class Book : Product
+    {
+        int pagine;
+
+        public Book(string titolo, int anno, string settore, string posizione, string[] autore, int pagine) : base(titolo, anno, settore, posizione, autore)
+        {
+            this.pagine = pagine;
+        }
+
+        public int Pagine { get => pagine; set => pagine = value; }
+
+        public override string ToString()
+        {
+            return base.ToString() + Environment.NewLine +
+                $"Pagine: {pagine}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -21,3 +21,53 @@
 //Creiamo anche una classe Biblioteca che contiene la lista dei documenti, la lista degli utenti e la lista dei prestiti.
 ////Contiene inoltre i metodi per le ricerche e per l’aggiunta dei documenti, utenti e prestiti. */
 
+using static Biblioteca;
+
+
+var users = new List<User>();
+var user1 = new User("Mattia", "Rossi", "mattiarossi@gmail.com", "qwerty", "1234567890");
+var user2 = new User("Giuseppe", "Verdi", "giuseppeverdi@gmail.com", "qwerty", "1234567890");
+var user3 = new User("Michele", "Bianchi", "michelebainchi@gmail.com", "qwerty", "1234567890");
+var user4 = new User("Alessandro", "Aranconi", "alessandroarancioni@gmail.com", "qwerty", "1234567890");
+var user5 = new User("Claudio", "Blu", "claudioblu@gmail.com", "qwerty", "1234567890");
+users.Add(user1);
+users.Add(user2);
+users.Add(user3);
+users.Add(user4);
+users.Add(user5);
+var books = new List<Book>();
+var book1 = new Book("La Bibbia", 200, "fantascienza", "F666", new[] { "Gesu", "Cristo" }, 2000);
+var book2 = new Book("Il Corano", 300, "comico", "H1", new[] { "Maometto", "Marocco" }, 5000);
+var book3 = new Book("La Torà", 100, "finanza", "S68", new[] { "Davide", "Giudeo" }, 2700);
+books.Add(book1);
+books.Add(book2);
+books.Add(book3);
+var DVDs = new List<DVD>();
+var DVD1 = new DVD("Star Wars", 1974, "fantascienza", "D57", new[] { "Chris", "Nolan" }, 180);
+var DVD2 = new DVD("Ritorno al Futuro", 1994, "drammatico", "F47", new[] { "John", "Johnny" }, 120);
+var DVD3 = new DVD("Il Signore Degli Anelli", 2001, "fantasy", "C69", new[] { "Steven", "Spielberg" }, 150);
+DVDs.Add(DVD1);
+DVDs.Add(DVD2);
+DVDs.Add(DVD3);
+
+Console.WriteLine("Lista Utenti: ");
+Console.WriteLine(ShowUsers(users));
+
+Console.WriteLine("Lista Libri: ");
+Console.WriteLine(ShowBooks(books));
+
+Console.WriteLine("Lista DVDs: ");
+Console.WriteLine(ShowDVDs(DVDs));
+
+string ShowUsers(List<User> list)
+{
+    return string.Join(Environment.NewLine, list).ToString();
+}
+string ShowBooks(List<Book> list)
+{
+    return string.Join(Environment.NewLine, list).ToString();
+}
+string ShowDVDs(List<DVD> list)
+{
+    return string.Join(Environment.NewLine, list).ToString();
+}
