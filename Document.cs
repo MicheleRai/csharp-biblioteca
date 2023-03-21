0633d8a9f01e6a074942a5e4b1b9c789dabb2e4b
MicheleRai <rainonemichele1999@gmail.com>
2023-03-21 15:37:45 +0100

    Add project files.

---
+++ Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Document
+    {
+        string id;
+        string titolo;
+        int anno;
+        string settore;
+        string scaffale;
+        string autore;
+    }
+}
